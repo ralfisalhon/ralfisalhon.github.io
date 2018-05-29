2037f5e58b9a2dc09637991b8f35030f1dcb9942
Ralfi Salhon <rifat.salhon@tufts.edu>
2018-05-29 16:42:12 +0300

    added easter egg

---
+++ assets/js/clicker.js
@@ -0,0 +1,66 @@
+// document.onkeydown = function(evt) {
+// 	evt = evt || window.event;
+// 	if (evt.keyCode == 82 && evt.keyCode == 65) {
+// 		clickCounter();
+// 	}
+// };
+
+var map = {82: false, 65: false, 76: false, 70: false, 73: false};
+$(document).keydown(function(e) {
+	if (e.keyCode in map) {
+		map[e.keyCode] = true;
+		if (map[82] && map[65] && map[76] && map[70] && map[73]) {
+			clickAppear();
+		}
+	}
+});
+
+function clickCounter() {
+	if(typeof(Storage) !== "undefined") {
+		if (localStorage.clickcount) {
+			localStorage.clickcount = Number(localStorage.clickcount)+1;
+		} else {
+			localStorage.clickcount = 1;
+		}
+
+		document.getElementById("before").innerHTML = "You have clicked this";
+		document.getElementById("result").innerHTML = "Button";
+		document.getElementById("after").innerHTML = localStorage.clickcount;
+		document.getElementById("after2").innerHTML = "Hide Me";
+
+		if (localStorage.clickcount == 1) {
+			document.getElementById("after").innerHTML += " time!";
+		} else {
+			document.getElementById("after").innerHTML += " times!"; 
+		}
+	}
+}
+
+function clickAppear() {
+	if(typeof(Storage) !== "undefined") {
+		if (localStorage.clickcount) {
+			localStorage.clickcount = Number(localStorage.clickcount);
+		} else {
+			localStorage.clickcount = 1;
+		}
+
+		document.getElementById("before").innerHTML = "You have clicked this";
+		document.getElementById("result").innerHTML = "Button";
+		document.getElementById("after").innerHTML = localStorage.clickcount;
+		document.getElementById("after2").innerHTML = "Hide Me";
+
+		if (localStorage.clickcount == 1) {
+			document.getElementById("after").innerHTML += " time!";
+		} else {
+			document.getElementById("after").innerHTML += " times!"; 
+		}
+	}
+}
+
+function hideCounter() {
+	document.getElementById("before").innerHTML = "";
+	document.getElementById("result").innerHTML = "";
+	document.getElementById("after").innerHTML = "";
+	document.getElementById("after2").innerHTML = "";
+	map = {82: false, 65: false, 76: false, 70: false, 73: false};
+}
