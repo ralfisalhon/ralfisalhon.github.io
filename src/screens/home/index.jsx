265b56393bfb59cb144290a2cdad9d36413d8242
Ralfi Salhon <ralfisalhon@gmail.com>
2020-04-13 22:20:33 -0400

    Rotating icons

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -15,24 +15,27 @@ class HomePage extends Component {
   render() {
     return (
       <center className="flexCenter">
-        <div className="row">
-          <img className="round_icon" alt="app_icon" src={coverlove} />
-          <img className="round_icon" alt="app_icon" src={igurme} />
-          <img className="round_icon" alt="app_icon" src={polysentry} />
-          <img className="round_icon" alt="app_icon" src={redorblue} />
-        </div>
         <div className="centertext">
           <h1 className="text">Hi, my name is Ralfi.</h1>
-          <p className="text">I develop mobile apps and websites, clean and efficiently.</p>
+          <p className="text">I develop great looking mobile apps and websites.</p>
         </div>
-
-        <div className="row">
-          <img className="round_icon" alt="app_icon" src={claimate} />
-          <img className="round_icon" alt="app_icon" src={hearo} />
-          <img className="round_icon" alt="app_icon" src={jamblr} />
-          <img className="round_icon" alt="app_icon" src={potencia} />
-          <img className="round_icon" alt="app_icon" src={tunewise} />
+        <div className="base">
+          <img className="item item-1 all" alt="app_icon" src={coverlove} />
+          <img className="item item-2 all" alt="app_icon" src={igurme} />
+          <img className="item item-3 all" alt="app_icon" src={polysentry} />
+          <img className="item item-4 all" alt="app_icon" src={redorblue} />
+          <img className="item item-5 all" alt="app_icon" src={claimate} />
+          <img className="item item-6 all" alt="app_icon" src={hearo} />
+          <img className="item item-7 all" alt="app_icon" src={jamblr} />
+          <img className="item item-8 all" alt="app_icon" src={potencia} />
+          {/* <img className="item item-9 all" alt="app_icon" src={tunewise} /> */}
         </div>
+        {/* <div class="base">
+          <div class="item red a1 all">1</div>
+          <div class="item green a2 all">2</div>
+          <div class="item blue a3 all">3</div>
+          <div class="item magenta a4 all">4</div>
+        </div> */}
       </center>
     );
   }
